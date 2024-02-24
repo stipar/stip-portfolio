98050783125fd18fce0a1546fee14ba0dce0b1c3
Christian Zaragoza <53922760+stipar@users.noreply.github.com>
2024-02-24 06:01:32 -0300

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,94 +1,87 @@
 type TSection = {
-  p: string;
-  h2: string;
-  content?: string;
+    p: string;
+    h2: string;
+    content?: string;
 };
 
 type TConfig = {
-  html: {
-    title: string;
-    fullName: string;
-    email: string;
-  };
-  hero: {
-    name: string;
-    p: string[];
-  };
-  contact: {
-    form: {
-      name: {
-        span: string;
-        placeholder: string;
-      };
-      email: {
-        span: string;
-        placeholder: string;
-      };
-      message: {
-        span: string;
-        placeholder: string;
-      };
+    html: {
+        title: string;
+        fullName: string;
+        email: string;
     };
-  } & TSection;
-  sections: {
-    about: Required<TSection>;
-    experience: TSection;
-    feedbacks: TSection;
-    works: Required<TSection>;
-  };
+    hero: {
+        name: string;
+        p: string[];
+    };
+    contact: {
+        form: {
+            name: {
+                span: string;
+                placeholder: string;
+            };
+            email: {
+                span: string;
+                placeholder: string;
+            };
+            message: {
+                span: string;
+                placeholder: string;
+            };
+        };
+    } & TSection;
+    sections: {
+        about: Required<TSection>;
+        experience: TSection;
+        feedbacks: TSection;
+        works: Required<TSection>;
+    };
 };
 
 export const config: TConfig = {
-  html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
-  },
-  hero: {
-    name: "John Doe",
-    p: ["I develop 3D visuals, user", "interfaces and web applications"],
-  },
-  contact: {
-    p: "Get in touch",
-    h2: "Contact.",
-    form: {
-      name: {
-        span: "Your Name",
-        placeholder: "What's your name?",
-      },
-      email: { span: "Your Email", placeholder: "What's your email?" },
-      message: {
-        span: "Your Message",
-        placeholder: "What do you want to say?",
-      },
+    html: {
+        title: "Christian Zaragoza — Portfolio",
+        fullName: "Christian Zaragoza",
+        email: "christianrobertozarargoza@gmail.com",
     },
-  },
-  sections: {
-    about: {
-      p: "Introduction",
-      h2: "Overview.",
-      content: `I'm a skilled software developer with experience in TypeScript and
-      JavaScript, and expertise in frameworks like React, Node.js, and
-      Three.js. I'm a quick learner and collaborate closely with clients to
-      create efficient, scalable, and user-friendly solutions that solve
-      real-world problems. Let's work together to bring your ideas to life!`,
+    hero: {
+        name: "Zaragoza Software Developer",
+        p: ["Desarrollo de aplicaciones web", "Desarrollo de aplicaicones multi plataforma"],
     },
-    experience: {
-      p: "What I have done so far",
-      h2: "Work Experience.",
+    contact: {
+        p: "Pongase en contacto",
+        h2: "Contacto.",
+        form: {
+            name: {
+                span: "STIP ''Soluciones Informáticas''",
+                placeholder: "¿Cuál es su nombre?",
+            },
+            email: { span: "Su Email", placeholder: "¿Cual es su E-Mail?" },
+            message: {
+                span: "Su Mensage",
+                placeholder: "What do you want to say?",
+            },
+        },
     },
-    feedbacks: {
-      p: "What others say",
-      h2: "Testimonials.",
+    sections: {
+        about: {
+            p: "Introducción",
+            h2: "Descripción general.",
+            content: `Soy un desarrollador de software capacitado con experiencia en C# y Python, experiencia en marcos como React, Node.js y Three.js. Aprendo rápido y colaboro estrechamente con los clientes para crear soluciones eficientes, escalables y fáciles de usar que resuelvan problemas del mundo real. ¡Trabajemos juntos para hacer realidad tus ideas!`,
+        },
+        experience: {
+            p: "Mis trabajos",
+            h2: "Experiencia Laboral.",
+        },
+        feedbacks: {
+            p: "LO QUE DICEN LOS DEMÁS",
+            h2: "Testimonios.",
+        },
+        works: {
+            p: "Mi Trabajo",
+            h2: "Projectos.",
+            content: `Los siguientes proyectos muestran mis habilidades y experiencia a través de ejemplos de mi trabajo del mundo real. Cada proyecto se describe brevemente con enlaces a repositorios de código y demostraciones en vivo. Refleja mi capacidad para resolver problemas complejos, trabajar con diferentes tecnologías y gestionar proyectos de forma eficaz.`,
+        },
     },
-    works: {
-      p: "My work",
-      h2: "Projects.",
-      content: `Following projects showcases my skills and experience through
-    real-world examples of my work. Each project is briefly described with
-    links to code repositories and live demos in it. It reflects my
-    ability to solve complex problems, work with different technologies,
-    and manage projects effectively.`,
-    },
-  },
 };
+
